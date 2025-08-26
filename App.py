a5bd7bea96146da8f52d90ba6fa17560268a80b3
Haanshacker <94171982+Haanshacker@users.noreply.github.com>
2025-08-26 14:36:26 +0500

    Update App.py

--- App.py
+++ App.py
@@ -2,7 +2,7 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
-import plotly.express as px
+import streamlit_plotly_events as px
 from datetime import datetime, timedelta, date
 
 st.set_page_config(page_title="CDR Analysis Dashboard", layout="wide")
